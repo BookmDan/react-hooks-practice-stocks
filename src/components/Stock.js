a2b033ecc671aabfc5a124862544b7e4ba30f121
BookmDan <danoh329@gmail.com>
2023-09-30 22:16:53 -0700

    stuck. stocks not showing

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Stock() {
+  const [stocks, setStocks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/stocks") // Adjust the URL based on your json-server setup
+      .then((response) => response.json())
+      .then((data) => setStocks(data.stocks));
+  }, []);
+
   return (
     <div>
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">{"Compant Name"}</h5>
-          <p className="card-text">{"Stock Price"}</p>
+      {stocks.map((stock) => (
+        <div key={stock.id} className="card">
+          <div className="card-body">
+            <h5 className="card-title">{stock.name}</h5>
+            <p className="card-text">Stock Price: ${stock.price}</p>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
+
 export default Stock;
