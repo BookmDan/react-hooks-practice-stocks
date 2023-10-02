b9a220822d6fce84f12abfe50e15de0032f6a2cc
BookmDan <danoh329@gmail.com>
2023-10-02 09:34:30 -0700

    did as far as I can

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,7 +1,34 @@
 import React, { useState, useEffect } from "react";
 
-function Stock() {
+
+function Portfolio({ myPortfolio }) {
+  return (
+    <div>
+      <h2>My Portfolio</h2>
+      <ul>
+        {myPortfolio.map((stock) => {
+          <li key={stock.id}>{stock.name} </li>
+        })}
+      </ul>
+    </div>
+  )
+}
+
+function Stock({ stock, onBuy }) {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">{stock.name}</h5>
+        <p className="card-text">Stock Price: ${stock.price}</p>
+        <button onClick={() => onBuy(stock)}>Buy</button>
+      </div>
+    </div>
+  );
+}
+
+function Stocks() {
   const [stocks, setStocks] = useState([]);
+  const [myPortfolio, setMyPortfolio] = useState([])
 
   useEffect(() => {
     fetch("http://localhost:4000/stocks") // Adjust the URL based on your json-server setup
@@ -9,18 +36,22 @@ function Stock() {
       .then((data) => setStocks(data.stocks));
   }, []);
 
+  const handleBuy = (stock) => {
+    setMyPortfolio((prevPortfolio) => [...prevPortfolio, stock])
+  };
+
   return (
     <div>
-      {stocks.map((stock) => (
-        <div key={stock.id} className="card">
-          <div className="card-body">
-            <h5 className="card-title">{stock.name}</h5>
-            <p className="card-text">Stock Price: ${stock.price}</p>
-          </div>
-        </div>
-      ))}
+      <h1>Stocks</h1>
+      <div>
+        {stocks.map((stock) => (
+          <Stock key={stock.id} stock={stock} onBuy={handleBuy} />
+        ))}
+      </div>
+      <hr />
+      <Portfolio myPortfolio={myPortfolio} />
     </div>
   );
 }
 
-export default Stock;
+export default Stocks;
